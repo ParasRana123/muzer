6cae2aa556016ae35159e2ae04c49185a69b372c
ParasRana123 <parasrana579@gmail.com>
2025-06-02 08:49:53 +0530

    Fixed some code

--- app/app/api/streams/upvote/route.ts
+++ app/app/api/streams/upvote/route.ts
@@ -1,5 +1,6 @@
+import { prismaClient } from "@/app/lib/db";
 import { getServerSession } from "next-auth";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 const UpvoteSchema = z.object({
@@ -9,5 +10,23 @@ const UpvoteSchema = z.object({
 
 export async function POST(req: NextRequest) {
     const session = getServerSession();
+    const user = await prismaClient.user.findFirst({
+        where: {
+            email: session?.user?.email ?? ""
+        }
+    });
+    if(!user) {
+        return NextResponse.json({
+            message: "Unauthenticated"
+        } , {
+            status: 403
+        })
+    }
+
+    try {
+
+    } catch () {
+
+    }
     const data = UpvoteSchema.parse(await req.json());
 }
